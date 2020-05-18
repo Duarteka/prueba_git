e82fdc4e4d06cdd0447b347d8cbda22c441d76a8
Karen Cristina duarte Aguiar <karencristinaduarteaguiar@MacBook-Pro.local>
2020-05-18 19:50:38 +0200

    este es un fichero python

---
+++ pruebas2.py
@@ -0,0 +1,49 @@
+"""def say_hi(name):
+  print("Hi there, " +name+ "!")
+say_hi("Hal")"""
+
+
+"""def to_fahrenheit(celsius):
+    f= celsius *9/5+32
+    return f
+degrees= to_fahrenheit(100)
+print (str(degrees) + "degrees")"""
+
+"""def to_miles(km):
+    miles = km / 1.609
+    return  km 
+    
+km=100 
+miles = to_miles(km)
+print (str(miles)+"miles")   
+
+def encode (numbers):
+  ns = []
+  for n in numbers:
+      ns.append(n * 8.15)
+  return   ns 
+numbers1= [4,8,15,16,23,42]
+numbers2= encode(numbers1)  
+print(numbers2)
+
+def say_hi(name):
+    print ("Hey there," +name+ "!")
+
+def greet (name, adj):
+    say_hi(name)
+    print("You look" +adj+ "!")
+
+greet("Luke", "great today")"""
+
+"""x= ("hola mundo")
+print (x)"""
+
+import random
+def esmayorque (my_number):
+    my_number=10
+    input ("Inserte un numero al azar:")
+    my_number.randomrange(100)
+    if (my_number>randonrange):
+        print ("El numero es mayor que:", (my_number))
+    else:
+        print("el numero es menor que:", (my_number))
